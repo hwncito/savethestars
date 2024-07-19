08ac4f022f7535ebfcd3c99a64fdf7cccba13c9d
hwncito <juanchosperoni@hotmail.com>
2024-07-19 19:18:13 -0300

    disabled animation for performance testing

--- src/components/hero/HeroButton.jsx
+++ src/components/hero/HeroButton.jsx
@@ -3,7 +3,7 @@
 export default function HeroButton() {
   return (
     <button
-      className="text-base md:text-lg border rounded-full py-3 px-8 md:px-10 font-medium hover:bg-white hover:text-slate-950 transition-all opacity-0 hero-button title-animation"
+      className="text-base md:text-lg border rounded-full py-3 px-8 md:px-10 font-medium hover:bg-white hover:text-slate-950 transition-all opacity-1 hero-button title-animation"
       onClick={() => {
         document
           .getElementById('supporters-section')
